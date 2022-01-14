0d17fed3701207c86a4986881bb14828a7acb12e
Carly Hayter <carlyhayter@gmail.com>
2022-01-14 14:01:05 -0800

    added options button and menu to outfitBaord card

--- components/OutfitBoardCard.js
+++ components/OutfitBoardCard.js
@@ -1,4 +1,17 @@
 import React from "react";
+import Image from "next";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import { styled } from "@mui/material/styles";
+import { ACTIONS, reducer } from "../utils/reducer";
 
 export default function OutfitBoardCard(props) {
   const { item } = props;
@@ -18,14 +31,68 @@ export default function OutfitBoardCard(props) {
   }
   shortBoolean = changeBoolean();
   console.log(shortBoolean);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handlePress = () => {
+    dispatch({ type: ACTIONS.CONSOLE_LOG });
+  };
+
+  const handleAddButtonClick = (sectionName) => {
+    dispatch({
+      type: ACTIONS.ADD_TO_SECTION,
+      payload: { item, section: sectionName },
+    });
+  };
   return (
     <div
       className={`outfit-board-card-inner-container${
         shortBoolean ? "-short" : ""
       }`}
       style={{ backgroundImage: `url(${item.src})` }}
-    ></div>
+    >
+      <IconButton
+        style={{ fontSize: 30 }}
+        aria-label="menu"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        className="closet-item-card-menu"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        <Tooltip title="More options">
+          <MoreHorizIcon
+            className="outfit-board-card-options"
+            fontSize="medium"
+          />
+        </Tooltip>
+      </IconButton>
+      <Menu
+        id="menu-dot"
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Edit item</MenuItem>
+        <MenuItem onClick={handleClose}>Delete from board</MenuItem>
+      </Menu>
+    </div>
   );
 }
 
